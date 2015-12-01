4ec50da5ffed620527cfb8b1f314cde6ce98a0c5
James Tyson <jbtyson@ksu.edu>
2015-11-30 19:22:59 -0600

    first attempt at calling a stored proc with params

--- server/services/db.js
+++ server/services/db.js
@@ -9,19 +9,30 @@ var Db = function(host, username, password, database) {
       database: database
     });
 
-    var query = function(query) {
+    var query = function(query, params) {
       var result;
-      connection.query(query, function(err, rows) {
-        if(err) {
-          console.log(err);
-          result = "NULL";
-        }
-        else {
-          result = rows;
-        }
-      });
+      if (!!params) {
+        connection.query(query, params, function(err, row) {
+          result = handleResults(err, rows);
+        });
+      }
+      else {
+        connection.query(query, function(err, rows) {
+          result = handleResults(err, rows);
+        });
+    }
 
       return result;
+    }
+
+    var handleResults = function(err, rows) {
+      if(err) {
+        console.log(err);
+        return "NULL";
+      }
+      else {
+        return rows;
+      }
     }
 
     return {
--- server/services/regionService/regionDbao.js
+++ server/services/regionService/regionDbao.js
@@ -1,4 +1,5 @@
-// regionDbao.js
+var Region = require("./../../game/region.js").Region;
+
 var RegionDbao = function(_db) {
     var db = _db
 
@@ -14,17 +15,48 @@ var RegionDbao = function(_db) {
 
     // Retrieve a region with a specificed id
     var retrieveRegion = function(regionId) {
+      var query = "CALL sp_retrieveRegion(?)";
+      var params = [regionId];
+      var result = db.query(query, params);
 
+      var region = new Region();
+      return region;
     }
 
     // Retrieve all regions currently active
     var retrieveAllRegions = function() {
+      var query = "CALL sp_retrieveAllRegions";
+      var results = db.query(query);
 
+      var regions;
+      if(!results || !results[0]) {
+        regions = "NULL"
+      }
+      else {
+        regions = [];
+        for(i = 0; i < results.length; i++) {
+          var region = new Region();
+          regions.puish(region);
+        }
+      }
+
+      return regions;
     }
 
     // Clear the regions in the db
     var clearRegions = function() {
+      var query = "CALL sp_clearRegions";
+      var results = db.query(query);
 
+      var success;
+      if(!results || !results.affectedRows) {
+        success = fale;
+      }
+      else {
+        success = true;
+      }
+
+      return success;
     }
 
     return {
